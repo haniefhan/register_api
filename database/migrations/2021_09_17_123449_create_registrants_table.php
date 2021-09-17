354c8f4a2461a8b05886e57528e20128095402bb
Hanief Han <haniefhan@gmail.com>
2021-09-17 19:43:46 +0700

    migration :: php artisan make:migration create_registrants_table; php artisan migrate

---
+++ database/migrations/2021_09_17_123449_create_registrants_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateRegistrantsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('registrants', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('id_card_number');
+            $table->text('address');
+            $table->string('phone');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('registrants');
+    }
+}
